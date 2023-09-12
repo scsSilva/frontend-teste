3fccf8f64592deb29203b17f206035ba9d1b682b
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 19:29:14 -0300

    feat: added global styles

---
+++ src/styles/global.tsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialised;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Inter', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`;
