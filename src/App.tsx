928033c62a919b31204abd26ec6955637fb48cfb
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-12 19:19:41 -0300

    feat: added routes wrapper in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import Router from "./router";
+
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   );
 };
 
