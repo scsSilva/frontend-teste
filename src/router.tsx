854914525705c1d3e3dca372c74b96d597c8e5c5
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-13 01:01:20 -0300

    feat: added palindrome page in routes

--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,17 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import DefaultLayout from "./layouts/DefaultLayout";
 import SearchCep from "./pages/SearchCep";
+import CashFlow from "./pages/CashFlow";
+import Palindrome from "./pages/Palindrome";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<DefaultLayout />}>
-        <Route path="/services" element={<SearchCep />} />
+        <Route path="/services/cep" element={<SearchCep />} />
+        <Route path="/services/cashFlow" element={<CashFlow />} />
+        <Route path="/services/palindrome" element={<Palindrome />} />
       </Route>
     </Routes>
   );
