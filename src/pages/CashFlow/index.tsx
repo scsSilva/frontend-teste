ce954a4c47d15e98bca584b60032b5812387e517
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-13 00:18:13 -0300

    feat: added CashFlow page

---
+++ src/pages/CashFlow/index.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react";
+import { useForm, FormProvider } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+import { api } from "../../utils/api";
+import Button from "../../components/Button";
+import Input from "../../components/Input";
+import Badge from "./components/Badge";
+import * as Styles from "./styles";
+
+const cashFlowFormValidationSchema = zod.object({
+  price: zod.number().min(1),
+  amountReceived: zod.number().min(1),
+});
+
+type CashFlowFormData = zod.infer<typeof cashFlowFormValidationSchema>;
+
+interface IChange {
+  price: number;
+  amountReceived: number;
+  change: number;
+  hundredNotes: number;
+  notesOutOfTen: number;
+  notesOutOfOne: number;
+}
+
+const CashFlow = () => {
+  const [change, setChange] = useState<IChange | null>(null);
+
+  const cashFlowForm = useForm<CashFlowFormData>({
+    resolver: zodResolver(cashFlowFormValidationSchema),
+  });
+
+  const { handleSubmit, watch, reset, register } = cashFlowForm;
+
+  const price = watch("price");
+  const amountReceived = watch("amountReceived");
+  const isSubmitDisabled = !(price && amountReceived && amountReceived > price);
+
+  const handleCalculateCash = async () => {
+    const response = await api.get(
+      `http://localhost:3000/cashFlow/change/${price}/${amountReceived}`
+    );
+
+    setChange(response.data);
+    reset();
+  };
+
+  return (
+    <Styles.Container>
+      <div>
+        <Styles.Title>Cash Flow</Styles.Title>
+      </div>
+
+      {change != null ? (
+        <Styles.ContentData>
+          <Styles.ListData>
+            <li>
+              <Badge title="Notas de 100" value={change.hundredNotes} />
+            </li>
+            <li>
+              <Badge title="Notas de 10" value={change.notesOutOfTen} />
+            </li>
+            <li>
+              <Badge title="Notas de 1" value={change.notesOutOfOne} />
+            </li>
+          </Styles.ListData>
+
+          <Button text="Voltar" onClick={() => setChange(null)} />
+        </Styles.ContentData>
+      ) : (
+        <Styles.Form onSubmit={handleSubmit(handleCalculateCash)}>
+          <FormProvider {...cashFlowForm}>
+            <Input
+              placeholder="Informe o valor da compra"
+              type="number"
+              min={1}
+              step={1}
+              pattern="/^\d+$/"
+              {...register("price", { valueAsNumber: true })}
+            />
+            <Input
+              placeholder="Informe o valor pago"
+              type="number"
+              min={1}
+              step={1}
+              pattern="/^\d+$/"
+              {...register("amountReceived", { valueAsNumber: true })}
+            />
+            <Button
+              text="Calcular"
+              disabled={isSubmitDisabled}
+              onClick={() => {}}
+            />
+          </FormProvider>
+        </Styles.Form>
+      )}
+    </Styles.Container>
+  );
+};
+
+export default CashFlow;
---
+++ src/pages/CashFlow/styles.ts
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background: beige;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem;
+
+  & > div {
+    height: 15%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    justify-self: flex-start;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+`;
+
+export const Form = styled.form`
+  flex: 1;
+  width: 25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  @media (max-width: 450px) {
+    width: 20rem;
+  }
+`;
+
+export const ContentData = styled.main`
+  flex: 1;
+  width: 25rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+
+  @media (max-width: 450px) {
+    width: 20rem;
+  }
+`;
+
+export const ListData = styled.ul`
+  list-style: none;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  & li {
+    width: 100%;
+  }
+`;
