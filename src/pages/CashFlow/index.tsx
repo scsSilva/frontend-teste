11bdeda99617d0f76ae154eaf8d18988928a71c3
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-13 16:31:59 -0300

    update index.tsx

--- src/pages/CashFlow/index.tsx
+++ src/pages/CashFlow/index.tsx
@@ -61,10 +61,7 @@ const CashFlow = () => {
               <Badge title="Valor da compra" value={change.price} />
             </li>
             <li>
-              <Badge
-                title="Troco"
-                value={change.amountReceived - change.price}
-              />
+              <Badge title="Troco" value={change.change} />
             </li>
             <li>
               <Badge title="Notas de 100" value={change.hundredNotes} />
