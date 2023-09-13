af0e5b0f6f1a1013d3a227380f3325f47f0facd0
Sallys Carlos <sallys.carlos93@gmail.com>
2023-09-13 00:18:21 -0300

    feat: added button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,17 @@
-import { ChangeEventHandler } from "react";
 import * as Styles from "./styles";
 
 interface ButtonProps {
   text: string;
   disabled?: boolean;
-  onClick: ChangeEventHandler;
+  onClick: () => void;
 }
 
 const Button = ({ text, disabled, onClick }: ButtonProps) => {
-  return <Styles.Container disabled={disabled}>{text}</Styles.Container>;
+  return (
+    <Styles.Container onClick={onClick} disabled={disabled}>
+      {text}
+    </Styles.Container>
+  );
 };
 
 export default Button;
